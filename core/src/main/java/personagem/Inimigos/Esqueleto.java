f3684b0e221f00e6a8622929789a72007fd8111c
Lucas Galarani <Lucasgalaranidc@gmail.com>
2026-06-04 13:40:55 -0300

    agoras personagem recebe dano ao encostar com inimigo

---
+++ core/src/main/java/personagem/Inimigos/Esqueleto.java
@@ -0,0 +1,10 @@
+package personagem.Inimigos;
+import com.badlogic.gdx.audio.Sound;
+import personagem.Inimigo;
+
+public class Esqueleto extends Inimigo {
+
+    public Esqueleto(float larguraMapa, float alturaMapa, float altura, float largura, Sound somDano) {
+        super(larguraMapa, alturaMapa, altura, largura, somDano);
+    }
+}
